c9fcc617c49cd5a236873d3dab730d0af7311121
cvsolsolcenfo <115435577+cvsolsolcenfo@users.noreply.github.com>
2022-10-20 11:27:01 -0600

    Creacion lista de usuarios

--- src/Controlador/Main.java
+++ src/Controlador/Main.java
@@ -1,6 +1,18 @@
 package Controlador;
-
-import Modelo.Usuarios;
-
+import Vista.*;
+import Modelo.*;
 public class Main {
+    public static void main (String []args){
+        boolean ok;
+
+        ok = programa.addUsuario(usuario);
+        if (ok == true) {
+            menu.mostrarTexto("Usuario en espera de ser ingresado");
+        } else {
+            break;
+        }
+        break;
+
+    }
+
 }
