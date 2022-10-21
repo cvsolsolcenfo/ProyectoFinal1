509e2c52b9ece9c945d7d195a6c762e6ac0f32e9
cvsolsolcenfo <115435577+cvsolsolcenfo@users.noreply.github.com>
2022-10-21 15:35:17 -0600

    Creacion conexion con sql server, segun rol

--- src/Controlador/Main.java
+++ src/Controlador/Main.java
@@ -1,18 +1,38 @@
 package Controlador;
-import Vista.*;
-import Modelo.*;
-public class Main {
-    public static void main (String []args){
-        boolean ok;
 
-        ok = programa.addUsuario(usuario);
-        if (ok == true) {
-            menu.mostrarTexto("Usuario en espera de ser ingresado");
-        } else {
-            break;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+//    public static void main (String []args){
+//        boolean ok;
+//
+//        ok = programa.addUsuario(usuario);
+//        if (ok == true) {
+//            menu.mostrarTexto("Usuario en espera de ser ingresado");
+//        } else {
+//            break;
+//        }
+//        break;
+//
+//    }
+//
+//}
+    public static Connection obtenerConexion (){
+        String url =";"
+                + "database=NOMBRE;"
+                +"user=sa;"
+                +"password=1234;";
+
+        try {
+            Connection con = DriverManager.getConnection(url);
+            return con;
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+            return null;
         }
-        break;
 
     }
-
 }
+
