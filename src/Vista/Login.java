a76b3c4a0a09ea6f69dbe23e3123e3e8747c03b7
cvsolsolcenfo <115435577+cvsolsolcenfo@users.noreply.github.com>
2022-10-21 18:52:50 -0600

    Creacion en scene builder de login, con ids

--- src/Vista/Login.java
+++ src/Vista/Login.java
@@ -1,24 +1,40 @@
-/**
- * Sample Skeleton for 'LoginVista.fxml' Controller Class
- */
-
-package Vista;
-
-import javafx.fxml.FXML;
+package Login;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-public class Login {
+public class Vista {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private TextField correo;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    void iniciarSesion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registrarse(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert correo != null : "fx:id=\"correo\" was not injected: check your FXML file 'LoginVista.fxml'.";
+        assert password != null : "fx:id=\"password\" was not injected: check your FXML file 'LoginVista.fxml'.";
 
     }
 
