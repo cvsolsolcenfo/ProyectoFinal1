f11b638df3f16c14ba45b04acd0abdb90c4a78e4
cvsolsolcenfo <115435577+cvsolsolcenfo@users.noreply.github.com>
2022-10-21 18:27:59 -0600

    Creacion en scene builde de login

---
+++ src/Vista/Login.java
@@ -0,0 +1,25 @@
+/**
+ * Sample Skeleton for 'LoginVista.fxml' Controller Class
+ */
+
+package Vista;
+
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Login {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
+
+}
